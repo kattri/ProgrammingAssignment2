4ad8ff96669991ac6e71ba9f57b9986de2d5299b
kattri <katri@gmail.com>
2015-02-22 14:16:32 +0530

    Limited comments to 80 columns
    Only some formatting

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,7 @@
 ## Takes  a square matrix, assuming non singula, and converts it into a special 
-## C++ class type object with its own cache variable soring the inverse of the object
-
+## C++ class type object with its own cache variable for  storing its inverse 
 ## Bundles a matrix with its inverse. There are methods to set the matrix, 
 ##extract and set its inverse.
-
 makeCacheMatrix <- function(x = matrix()) {  
   m <- NULL
   set <- function(y) {
@@ -19,11 +17,10 @@ makeCacheMatrix <- function(x = matrix()) {
   
   }
 
-## Function which when called on special bundled object created using makeCacheMatrix
-##checks for if the the special bundled matrix has its inverse already calculated or not
-## in case the invers is not a null the same is returned else it is calcualted and 
+## Function  when called on special bundled object created using makeCacheMatrix
+##checks for if the the special bundled matrix has  inverse already  or not
+## in case the inverse is not a null  same is returned else it is calcualted and 
 ##stored in the bundled onject.
-
 cacheSolve <- function(x, ...) {
      m <- x$getinv()
      if(!is.null(m)) {
@@ -34,5 +31,6 @@ cacheSolve <- function(x, ...) {
      m <- solve(data, ...)
      x$setinv(m)
      m      
-##Return a matrix that is the inverse of 'x'Verify taking a diag of original and inverse
+##Return a matrix that is the inverse of 'x'Verify taking a diag of original 
+##and inverse
 }
