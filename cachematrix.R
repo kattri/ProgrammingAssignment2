1d9c72db72a1e540fb1c0538ab39589b457fdef8
kattri <katri@gmail.com>
2015-02-22 13:45:38 +0530

    Completed Assignment for the Programming Assignment
    Essentially as per the template in the assignment. Just checked up that
    inverse and matrix multiply back to a diagonal matrix. Diag is an
    intersting command so is that %*% for two mat mult. Remided of C++
    objects.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Takes  a square matrix, assuming non singula, and converts it into a special 
+## C++ class type object with its own cache variable soring the inverse of the object
 
-## Write a short comment describing this function
+## Bundles a matrix with its inverse. There are methods to set the matrix, 
+##extract and set its inverse.
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x = matrix()) {  
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inv) m <<- inv
+  getinv <- function() m
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
+  
+  }
 
-}
-
-
-## Write a short comment describing this function
+## Function which when called on special bundled object created using makeCacheMatrix
+##checks for if the the special bundled matrix has its inverse already calculated or not
+## in case the invers is not a null the same is returned else it is calcualted and 
+##stored in the bundled onject.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+     m <- x$getinv()
+     if(!is.null(m)) {
+          message("getting cached data")
+          return(m)
+     }
+     data <- x$get()
+     m <- solve(data, ...)
+     x$setinv(m)
+     m      
+##Return a matrix that is the inverse of 'x'Verify taking a diag of original and inverse
 }
